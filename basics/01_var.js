34e20d9873a835123cd2c9f932261e9ff6777597
Aman Prajapati <pandaanimal55555@gmail.com>
2026-07-04 07:30:01 +0000

    variables concept

---
+++ basics/01_var.js
@@ -0,0 +1,16 @@
+const accId = 12312
+let myEmail = "amanmp@gmail.com" //correct
+var accPassword = "Aman123" // NOT PREFFRED
+accCity = "Gandhinagar" // correct
+let accbank
+myEmail = 1234
+accbank = "BOB"
+// accId = 32
+// myEmail = "aman@yaahoo.com"
+accPassword = "123"
+accCity = "Ahemdabad"
+console.log(myEmail);
+console.log(accPassword);
+console.log(accCity);
+
+console.table([accId,myEmail, accPassword, accCity, accbank])

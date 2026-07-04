9723b6c94684976160d369f6d6b189ebf71bbd57
Aman Prajapati <pandaanimal55555@gmail.com>
2026-07-04 08:45:17 +0000

    datatypes learnt

---
+++ basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code in newer version
+
+// alert(3+3) // it's for browser not node.js
+
+let name = "Aman"
+let age = 21
+let islegal = true
+
+console.table([typeof name,typeof age,typeof islegal])
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+let a = null;
+let b;
+
+console.table([typeof a, typeof b])
+// null gives object as type, undefined will give undefined as type.
